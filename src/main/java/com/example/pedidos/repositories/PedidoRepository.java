29b1f84b58f45556ee4e1c34d1795f2388c5cf76
Maria <dudahpereira13@gmail.com>
2026-03-22 21:14:07 -0300

    repository criado

---
+++ src/main/java/com/example/pedidos/repositories/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.example.pedidos.repositories;
+
+import com.example.pedidos.models.PedidoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
+}
